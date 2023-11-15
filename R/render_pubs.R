1944a501c248838028703a172a1897c03e2ed25c
David Beauchesne <david.beauchesne@uqar.ca>
2023-11-15 12:18:01 -0500

    extract data for health regions

--- R/render_pubs.R
+++ R/render_pubs.R
@@ -4,45 +4,62 @@
 render_report <- function() {
   # file.copy("./figures/", "./report/", recursive = TRUE)
   suppressWarnings({
-    setwd('./pubs/report/')
+    setwd("./pubs/report/")
     bookdown::render_book(
       input = "index.Rmd",
       output_format = "bookdown::gitbook",
       config_file = "_bookdown.yml"
     )
-    setwd('../../')
+    setwd("../../")
     # file.copy("./figures/", "./report/", recursive = TRUE)
   })
-  from <- here::here("pubs","report","docs")
+  from <- here::here("pubs", "report", "docs")
   # to <- here::here("docs","report")
   to <- here::here("docs")
   if (file.exists(to)) fs::file_delete(to)
   fs::file_move(from, to)
 }
 
-#' Render publications frontpage 
+#' Render publications frontpage
 #'
 #' @export
 render_frontpage <- function() {
-    setwd('./pubs/frontpage/')
-    rmarkdown::render("index.Rmd")
-    setwd('../../')
-    out <- here::here("docs")
-    chk_create(out)
-    files <- list.files(here::here("pubs","frontpage"), full.names = TRUE)
-    file.copy(from = files, to = out, recursive = TRUE)
+  setwd("./pubs/frontpage/")
+  rmarkdown::render("index.Rmd")
+  setwd("../../")
+  out <- here::here("docs")
+  chk_create(out)
+  files <- list.files(here::here("pubs", "frontpage"), full.names = TRUE)
+  file.copy(from = files, to = out, recursive = TRUE)
 }
 
 #' Render webinar
 #'
 #' @export
 render_webinar <- function() {
-    setwd('./pubs/webinar/')
-    rmarkdown::render("index.Rmd")
-    setwd('../../')
-    out <- here::here("docs","webinar")
-    chk_create(out)
-    files <- list.files(here::here("pubs","webinar"), full.names = TRUE)
-    file.copy(from = files, to = out, recursive = TRUE)
+  setwd("./pubs/webinar/")
+  rmarkdown::render("index.Rmd")
+  setwd("../../")
+  out <- here::here("docs", "webinar")
+  chk_create(out)
+  files <- list.files(here::here("pubs", "webinar"), full.names = TRUE)
+  file.copy(from = files, to = out, recursive = TRUE)
 }
 
+
+#' Render manuscript
+#'
+#' @export
+render_manuscript <- function() {
+  # file.copy("./figures/", "./report/", recursive = TRUE)
+  rmarkdown::render("pubs/manuscript/covidfn.Rmd")
+}
+
+
+#' Render manuscript supplementary material
+#'
+#' @export
+render_manuscript_sm <- function() {
+  # file.copy("./figures/", "./report/", recursive = TRUE)
+  rmarkdown::render("pubs/manuscript/covidfn_sm.Rmd")
+}
