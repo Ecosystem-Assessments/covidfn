06fbef62552f9bed47d70a896ee294b266586a66
David Beauchesne <david.beauchesne@uqar.ca>
2024-02-20 00:10:20 -0500

    glm figures

--- R/fig_glm_bn_multi.R
+++ R/fig_glm_bn_multi.R
@@ -46,8 +46,8 @@ fig_glm_bn_multi <- function() {
         dat[i],
         col = viridis::viridis(sum(p > 0)),
         breaks = p,
-        axes = TRUE,
-        main = name[[i]]
+        axes = FALSE,
+        main = NULL
       )
       dev.off()
     }
@@ -110,22 +110,35 @@ fig_glm_bn_multi <- function() {
         pointsize = 10
       )
       par(
-        mar = c(1, 1, 1, 2)
+        mar = c(0, 1, 0, 1)
       )
-      graphicsutils::plot0(x = c(0, nCol + 1), y = c(-nRow - 2, 0))
+      xlim <- c(.25, nCol + 1)
+      graphicsutils::plot0(x = xlim, y = c(-nRow - 2, 2))
       x <- c(1, 3.5, 4, 4.5)
 
+      # Table titles & model summary
+      text(x = mean(xlim), y = 2.5, labels = "Generalized Linear Models", adj = .5, cex = 1.25, font = 2)
+      text(
+        x = mean(xlim), y = 1.9, adj = .5, cex = 1,
+        labels = "Negative binomial distribution - offset = log(population size)"
+      )
+      text(x = mean(xlim) / 2, y = 1, labels = glue::glue("Number of parameters: {nrow(tmp[[i]])}"), adj = .5, font = 2)
+      text(x = mean(xlim), y = 1, labels = latex2exp::TeX(glue::glue("McFadden's pseudo-$R^2$: {round(r2$R2[i],2)}"), bold = TRUE), adj = .5)
+      text(x = 3 * (mean(xlim) / 2), y = 1, labels = glue::glue("AIC: {round(r2$AIC[i],2)}"), adj = .5, font = 2)
+
       # Column names
-      text(x = x[1], y = 0, labels = "Group", adj = .5, cex = 1.25, font = 2)
-      text(x = x[2], y = 0, labels = "Indicators", adj = c(1, .5), cex = 1.25, font = 2)
-      text(x = x[3], y = 0, labels = "Coefficients", adj = .5, cex = 1.25, font = 2)
-      text(x = x[4], y = 0, labels = "p value", adj = .5, cex = 1.25, font = 2)
+      text(x = x[1], y = 0, labels = "Group", adj = .5, cex = 1, font = 2)
+      text(x = x[2], y = 0, labels = "Indicators", adj = c(1, .5), cex = 1, font = 2)
+      text(x = x[3], y = 0, labels = "Coefficients", adj = .5, cex = 1, font = 2)
+      text(x = x[4], y = 0, labels = "p value", adj = .5, cex = 1, font = 2)
 
       # Groups
       text(x = rep(x[1], nrow(groups)), y = -groups$pos, adj = .5, labels = groups$group, font = 1, cex = 1)
       for (k in -seq_len(nrow(groups))) {
         lines(x = c(x[1] - xG, x[4] + xG / 2), y = rep(-groups$line[-k], 2), col = "#00000066")
       }
+      lines(x = c(x[1] - xG, x[4] + xG / 2), y = rep(0 + lG, 2), col = "#000000", lwd = 1.5)
+      lines(x = c(x[1] - xG, x[4] + xG / 2), y = rep(-nrow(tmp[[i]]) - lG, 2), col = "#000000", lwd = 1.5)
 
       # Rest of text
       for (k in -seq_len(nrow(tmp[[i]]))) {
@@ -140,21 +153,79 @@ fig_glm_bn_multi <- function() {
   plot_mod(coefs)
 
   # # ----------------------------------------------------------------------------------------------------------------
-  # # Combine everything with image magick
-  # img <- list()
-  # for (i in seq_len(nrow(r2))) {
-  #   i1 <- magick::image_read(here::here(out2, glue::glue("spatial_{r2$y[i]}.png")))
-  #   i2 <- magick::image_read(here::here(out2, glue::glue("models_{r2$y[i]}.png")))
-  #   img[[i]] <- magick::image_append(c(i1, i2), stack = TRUE)
-  # }
+  # Functions
+  nm_title <- function(img, chr) {
+    wd <- magick::image_info(img)$width * .5
+    # ht <- magick::image_info(img)$height * .00001
+    magick::image_annotate(
+      img,
+      chr,
+      # location = glue::glue("+{wd}+0"),
+      gravity = "north",
+      size = 75,
+      font = "Palatino",
+      style = "Italic",
+      weight = 2000,
+      # decoration = "underline",
+      color = NULL,
+    )
+  }
 
-  # # Cases
-  # cases <- list(
-  #   magick::image_append(c(img[[1]], img[[2]]), stack = FALSE),
-  #   magick::image_append(c(img[[3]], img[[4]]), stack = FALSE)
-  # )
-  # cases <- magick::image_append(c(cases[[1]], cases[[2]]), stack = TRUE)
-  # magick::image_write(cases, here::here(out, "glm_nb_multi_cases.png"))
-  # rm(cases)
-  # gc()
+  nm_sub <- function(img, chr) {
+    wd <- magick::image_info(img)$width * .5
+    magick::image_annotate(
+      img,
+      chr,
+      location = "+0+100",
+      gravity = "north",
+      size = 75,
+      font = "Palatino",
+      style = "Italic",
+      weight = 200,
+      color = "#494949",
+    )
+  }
+
+  # Combine everything with image magick
+  img <- list()
+  for (i in seq_len(nrow(r2))) {
+    # Images
+    i1 <- magick::image_read(here::here(out2, glue::glue("spatial_{r2$y[i]}.png")))
+    i2 <- magick::image_read(here::here(out2, glue::glue("models_{r2$y[i]}.png")))
+
+    # Border to spatial image
+    i1 <- magick::image_resize(i1, "50%x50%")
+    wd <- (magick::image_info(i2)$width - magick::image_info(i1)$width) / 2
+    i1 <- magick::image_border(i1, glue::glue("{wd}x150"), color = "#ffffff")
+
+    # Combine
+    img[[i]] <- magick::image_append(c(i1, i2), stack = TRUE)
+
+    # Add title & border
+    name <- stringr::str_split(r2$y[i], "_") |>
+      unlist()
+    img[[i]] <- nm_title(img[[i]], glue::glue("Number of {name[1]}")) |>
+      nm_sub(glue::glue("{name[2]}-{name[3]}-{name[4]} to {name[5]}-{name[6]}-{name[7]}")) |>
+      magick::image_border("1x1", color = "#000000")
+  }
+
+  # Cases
+  cases <- list(
+    magick::image_append(c(img[[1]], img[[2]]), stack = FALSE),
+    magick::image_append(c(img[[3]], img[[4]]), stack = FALSE)
+  )
+  cases <- magick::image_append(c(cases[[1]], cases[[2]]), stack = TRUE)
+  magick::image_write(cases, here::here(out, "glm_nb_multi_cases.png"))
+  rm(cases)
+  gc()
+
+  # deaths
+  deaths <- list(
+    magick::image_append(c(img[[5]], img[[6]]), stack = FALSE),
+    magick::image_append(c(img[[7]], img[[8]]), stack = FALSE)
+  )
+  deaths <- magick::image_append(c(deaths[[1]], deaths[[2]]), stack = TRUE)
+  magick::image_write(deaths, here::here(out, "glm_nb_multi_deaths.png"))
+  rm(deaths)
+  gc()
 }
